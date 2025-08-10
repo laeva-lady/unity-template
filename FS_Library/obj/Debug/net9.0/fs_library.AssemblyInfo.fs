db1b53642fe317c6404f7ae33504f4c82699d93c
Lyna <alicelegare@outlook.com>
2025-08-10 17:37:52 -0400

    change location of the DLLs

--- FS_Library/obj/Debug/net9.0/fs_library.AssemblyInfo.fs
+++ FS_Library/obj/Debug/net9.0/fs_library.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("fs_library")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df66c2f785f8a62fd72ac56540251a12f969cc3")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("fs_library")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("fs_library")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
